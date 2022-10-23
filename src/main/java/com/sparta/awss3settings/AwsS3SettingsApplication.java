be1e09cdd48884ae44e41250e78491cd9c14eba1
RefinedStone <chem.en.9273@gmail.com>
2022-10-23 14:23:37 +0900

    aws-s3-sample-code report

---
+++ src/main/java/com/sparta/awss3settings/AwsS3SettingsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.awss3settings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3SettingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsS3SettingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/awss3settings/AwsS3SettingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.awss3settings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3SettingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
